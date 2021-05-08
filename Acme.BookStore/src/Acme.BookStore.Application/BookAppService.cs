b35da7cbac4e5be5be7eb08c072cae0904ab380b
zpy <12537@DESKTOP-8ERVNII>
2021-05-08 15:38:56 +0800

    提交了abp代码

---
+++ Acme.BookStore/src/Acme.BookStore.Application/BookAppService.cs
@@ -0,0 +1,28 @@
+﻿using Acme.BookStore.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore
+{
+    public class BookAppService :
+        CrudAppService<
+            Book,
+            BookDto,
+            Guid,         
+            PagedAndSortedResultRequestDto,
+            CreateUpdateBookDto>,
+        IBookAppService
+    {
+        public BookAppService(IRepository<Book,Guid> repository)
+            : base(repository)
+        {
+            
+        }
+    }
+}
